0e8b4ccc188af48b621556290bb3853df5b515de
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-15 18:08:55 +0500

    feat: create routes for image, document, profile controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// DOCUMENTS ROUTES
+Route::get('/documents', [DocumentController::class, 'index'])->name('documents.index');
+Route::post('/documents', [DocumentController::class, 'store'])->name('documents.store');
+Route::delete('/documents/{document}', [DocumentController::class, 'destroy'])->name('documents.destroy');
+
+// IMAGES ROUTES
+Route::get('/images', [ImageController::class, 'index'])->name('images.index');
+Route::post('/images', [ImageController::class, 'store'])->name('images.store');
+Route::delete('/images/{image}', [ImageController::class, 'destroy'])->name('images.destroy');
+
+// PROFILES ROUTES
+Route::get('/profiles', [ProfileController::class, 'index'])->name('profiles.index');
+Route::post('/profiles', [ProfileController::class, 'store'])->name('profiles.store');
+Route::delete('/profiles/{profile}', [ProfileController::class, 'destroy'])->name('profiles.destroy');
