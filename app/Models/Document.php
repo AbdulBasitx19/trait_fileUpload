30cba457808ab04da96d768fff3ac417fe864802
AbdulBasitx19 <abdulbasitchaudhry106@gmail.com>
2026-09-15 16:54:34 +0500

    feat: create Document model and added fillables for Document uploading

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Document extends Model
 {
     //
+    protected $fillable =[
+        'title',
+        'original_name',
+        'file_path',
+        'disk',
+        'mime_type',
+        'file_size'
+    ];
 }
